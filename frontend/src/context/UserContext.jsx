44781a56611a27870c9fd666b64faf44891ca845
priyanshutiwari09 <priyanshuganeshtiwari@gmail.com>
2025-07-30 16:57:24 +0530

    updated UserContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 export const userDataContext = createContext();
 
 const UserContext = ({ children }) => {
   const serverUrl = "http://localhost:5000";
+  const [userData, setUserData] = useState(null);
+
+  const handleCurrentUser = async () => {
+    try {
+      const res = await axios.get(`${serverUrl}/api/user/current`, {
+        withCredentials: true
+      });
+
+      setUserData(res.data);
+      // console.log(userData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleCurrentUser();
+  }, []);
+
+  // 👇 log userData when it actually changes
+  useEffect(() => {
+    console.log("Updated userData:", userData);
+  }, [userData]);
+
   const value = {
     serverUrl
   };
