f695e208db79d254cb02129938e08315fdbd4b8e
priyanshutiwari09 <priyanshuganeshtiwari@gmail.com>
2025-08-11 19:53:44 +0530

    edited homejs

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -28,13 +28,13 @@ const UserContext = ({ children }) => {
 
   const getGeminiResponse = async (command) => {
     try {
-      const result = axios.post(
+      const result = await axios.post(
         `${serverUrl}/api/user/asktoassistant`,
         { command },
         { withCredentials: true }
       );
 
-      return (await result).data;
+      return result.data;
     } catch (error) {
       console.log(error);
     }
